3eb0f58c9cf7d26264de9e92f78b99bc95bd55d8
Chengyu Dai <aacherythdai@gmail.com>
2021-04-03 22:28:02 -0700

    commit on 'main'

--- password.js
+++ password.js
@@ -4,51 +4,48 @@ var numericCharacters = "0123456789";
 var symbolCharacters = "!@#$%^&*()_+\/?<>";
 var lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
 var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charSelected = ["numbericCharacters" + "symbolCharacters" + "lowerCharacters" + "upperCharacters"];
 
 
 // Write password to the #password input
-function writePassword(length) {
-  var length = window.prompt("Enter the counts of characters for your password?\nPlease choose between 8 to 128 characters.");
-  if (length < 8) {
-    alert("Must be at least 8 characters");
-    return;
-  } else if (length > 128) {
-    alert("Must not have more than 128 characters");
-    return;
-  } else {
-    var charType1 = confirm("Must have at least one number.");
-    if (confirm ("Press OK to Confirm")) {
-      text = "You pressed OK!";
-    } else {
-      text = "You pressed Cancel";
-      return;
-    }
-  } var charType2 = confirm("Must have at least one symbol.");
-  if (confirm ("Press OK to Confirm")) {
-    text = "You pressed OK!";
-  } else {
-    text = "You pressed Cancel";
-    return;
-  } var charType3 = confirm("Must have at least one lowercase letter.");
-  if (confirm ("Press OK to Confirm")) {
-    text = "You pressed OK!";
-  } else {
-    text = "You pressed Cancel";
-    return;
-  } var charType3 = confirm("Must have at least one uppercase letter.");
-  if (confirm ("Press OK to Confirm")) {
-    text = "You pressed OK!";
-  } else {
-    text = "You pressed Cancel";
-    return;
-  }
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#generate");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+
+function generatePassword(length) {
+    var length = window.prompt("Enter the number of characters for your password?\nPlease choose between 8 to 128 characters.");
+        if (length > 7 || length < 129) {
+        var numType = confirm("Click 'OK' to confirm having numeric characters");
+        var symType = confirm("Click 'OK' to confirm having symbolic characters");
+        var lowType = confirm("Click 'OK' to confirm having lowercase letters");
+        var upType = confirm("Click 'OK' to confirm having uppercase letters");
+            if (numType == true || symType == true || lowType == true || upType == true) {
+                if (numType == true) {
+                    charSelected += numericCharacters;
+                }
+                if (symType == true) {
+                    charSelected += symbolCharacters;
+                }
+                if (lowType == true) {
+                    charSelected += lowerCharacters;
+                }
+                if (upType == true) {
+                    charSelected += upperCharacters;
+                }
+            }
+        }
+};
+
+function writePassword() {
+    var password = generatPassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+    var charTypes = charSelected;
+    var charLength = charTypes.length;
+    for (var i = 0; i < charTypes.length; i++) {
+        result += charTypes(Math.floor(Math.random()*charLength));
+    }
+}
