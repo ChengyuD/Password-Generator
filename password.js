39c827c62d8babc0bc4ebbeda7338c799cede018
Chengyu Dai <aacherythdai@gmail.com>
2021-04-05 11:17:53 -0700

    commit on 'main'

--- password.js
+++ password.js
@@ -4,48 +4,46 @@ var numericCharacters = "0123456789";
 var symbolCharacters = "!@#$%^&*()_+\/?<>";
 var lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
 var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var charSelected = ["numbericCharacters" + "symbolCharacters" + "lowerCharacters" + "upperCharacters"];
+var charSelected = "";
+var passwordEl = [];
 
-
-// Write password to the #password input
-
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
 
-function generatePassword(length) {
-    var length = window.prompt("Enter the number of characters for your password?\nPlease choose between 8 to 128 characters.");
-        if (length > 7 || length < 129) {
+// prompt and confirmation of length and character types after clicking the generate password button
+generateBtn.addEventListener("click", generatePassword);
+
+function generatePassword() {
+    var length = window.prompt("Enter the number of characters for your password?\nPlease choose between 8 and 128 characters.");
+    if (length > 7 || length < 129) {
         var numType = confirm("Click 'OK' to confirm having numeric characters");
         var symType = confirm("Click 'OK' to confirm having symbolic characters");
         var lowType = confirm("Click 'OK' to confirm having lowercase letters");
         var upType = confirm("Click 'OK' to confirm having uppercase letters");
-            if (numType == true || symType == true || lowType == true || upType == true) {
-                if (numType == true) {
-                    charSelected += numericCharacters;
-                }
-                if (symType == true) {
-                    charSelected += symbolCharacters;
-                }
-                if (lowType == true) {
-                    charSelected += lowerCharacters;
-                }
-                if (upType == true) {
-                    charSelected += upperCharacters;
-                }
+        if (numType == true || symType == true || lowType == true || upType == true) {
+            if (numType == true) {
+                charSelected += numericCharacters;
+            }
+            if (symType == true) {
+                charSelected += symbolCharacters;
+            }
+            if (lowType == true) {
+                charSelected += lowerCharacters;
+            }
+            if (upType == true) {
+                charSelected += upperCharacters;
             }
         }
+
+        for (var i = 0; i < length; i++) {
+            password = charSelected + passwordEl[Math.floor(Math.random()*passwordEl.length)];
+            console.log(password);
+        }
 };
+
 
 function writePassword() {
     var password = generatPassword();
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-
-    var charTypes = charSelected;
-    var charLength = charTypes.length;
-    for (var i = 0; i < charTypes.length; i++) {
-        result += charTypes(Math.floor(Math.random()*charLength));
-    }
-}
+};
