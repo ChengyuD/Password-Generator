b927381da8cee478c49063c83b527e69ff1c6e76
Cheryth D <80147201+ChengyuD@users.noreply.github.com>
2021-04-08 15:16:03 -0700

    Update password.js

--- password.js
+++ password.js
@@ -1,51 +1,68 @@
-// Assignment Code
+var generatePassword = function() {
+    var password = "";
+
+    var charLength = passwordLength();
+    if (charLength < 8 || charLength > 128 || charLength === "") {
+        alert("Password length must be between 8 to 128 characters!");
+        return;
+    }
+
+    var charSet = passwordSet();
+
+    var charSetLength = charSet.length;
+    console.log(charLength, charSet, charSetLength)
+
+    for (var i = 0; i < charLength; i++) {
+       password += charSet.charAt(Math.floor(Math.random()*charSetLength));
+    }
+    console.log(password)
+    return password;
+}
+
+var passwordLength = function() {
+    var lengthChar = prompt("Enter the length of characters for your password?\nPassword length must be between 8 to 128 characters.");
+
+    lengthChar = Math.floor(Number(lengthChar));
+    return lengthChar;
+}
+
+var passwordSet = function() {
+    var charSet = "";
+
+    var numbers = "0123456789";
+    var symbols = "!@#$%^&*()_+\/?<>";
+    var lowerCases = "abcdefghijklmnopqrstuvwxyz";
+    var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var numChar = confirm("Click 'OK' to confirm having numeric characters");
+    var symChar = confirm("Click 'OK' to confirm having symbolic characters");
+    var lowerChar = confirm("Click 'OK' to confirm having lowercase letters");
+    var upperChar = confirm("Click 'OK' to confirm having uppercase letters");
+        
+    if (numChar === true) {
+        charSet += numbers;
+    }
+    if (symChar === true) {
+        charSet += symbols;
+    }
+    if (lowerChar === true) {
+        charSet += lowerCases;
+    }
+    if (upperChar === true) {
+        charSet += upperCases;
+    }
+    return charSet;
+}
+
 var generateBtn = document.querySelector("#generate");
-var numericCharacters = "0123456789";
-var symbolCharacters = "!@#$%^&*()_+\/?<>";
-var lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
-var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var charSelected = ["numbericCharacters" + "symbolCharacters" + "lowerCharacters" + "upperCharacters"];
-
-
-// Write password to the #password input
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
-
-function generatePassword(length) {
-    var length = window.prompt("Enter the number of characters for your password?\nPlease choose between 8 to 128 characters.");
-        if (length > 7 || length < 129) {
-        var numType = confirm("Click 'OK' to confirm having numeric characters");
-        var symType = confirm("Click 'OK' to confirm having symbolic characters");
-        var lowType = confirm("Click 'OK' to confirm having lowercase letters");
-        var upType = confirm("Click 'OK' to confirm having uppercase letters");
-            if (numType == true || symType == true || lowType == true || upType == true) {
-                if (numType == true) {
-                    charSelected += numericCharacters;
-                }
-                if (symType == true) {
-                    charSelected += symbolCharacters;
-                }
-                if (lowType == true) {
-                    charSelected += lowerCharacters;
-                }
-                if (upType == true) {
-                    charSelected += upperCharacters;
-                }
-            }
-        }
-};
 
 function writePassword() {
-    var password = generatPassword();
-    var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    console.log(password)
+    var passwordText = document.getElementById("password");
 
     passwordText.value = password;
+};
 
-    var charTypes = charSelected;
-    var charLength = charTypes.length;
-    for (var i = 0; i < charTypes.length; i++) {
-        result += charTypes(Math.floor(Math.random()*charLength));
-    }
-}
+generateBtn.addEventListener("click", writePassword);
+
